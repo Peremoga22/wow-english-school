1c951778b409e4288602ed389579742e6a53f0e0
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-12-18 00:47:32 +0200

    add culture icon

---
+++ WowApp/wwwroot/culture.js
@@ -0,0 +1,20 @@
+﻿window.setCulture = function (culture, returnUrl) {
+    const form = document.createElement("form");
+    form.method = "post";
+    form.action = "/set-culture";
+
+    const c = document.createElement("input");
+    c.type = "hidden";
+    c.name = "culture";
+    c.value = culture;
+
+    const r = document.createElement("input");
+    r.type = "hidden";
+    r.name = "returnUrl";
+    r.value = returnUrl;
+
+    form.appendChild(c);
+    form.appendChild(r);
+    document.body.appendChild(form);
+    form.submit();
+};
